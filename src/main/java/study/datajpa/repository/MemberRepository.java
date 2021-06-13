8acdc97717854240e834d433294781991e33d402
ggozlo <ggozlo@naver.com>
2021-06-14 00:49:47 +0900

    반환타입, 페이징, 정렬, 벌크성 수정쿼리, 엔티티 그래프

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,13 +1,19 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -28,4 +34,37 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    @Query("select  m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+    List<Member> findListByUsername(String username);
+
+    Member findMemberByUsername(String username);
+
+    Optional<Member> findOptionalByUsername(String username);
+
+    @Query(value = "select m from Member m left join m.team t", countQuery = "select count(m.username) from Member m")
+    // 조회 쿼리가 복잡해질 경우 카운트 쿼리의 성능이 저하될수 있다. 이경우 쿼리 어노테이션으로 카운트용 쿼리를 분리할수 있다.
+    Page<Member> findByAge(int age, Pageable pageable);
+    // Slice, Page, List
+
+    @Modifying(clearAutomatically = true) // .executeUpdate() 메서드랑 같은 역할
+    @Query("update Member m set m.age = m.age +1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"}) // 페치조인 기능 어노테이션, 옵션에 들어간 연관관계를 페치조인 하는듯...
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m ")
+    List<Member> findMemberEntityGraph();
+
+  //  @EntityGraph(attributePaths = ("team"))
+    @EntityGraph("Member.all")
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,14 +5,20 @@ import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,6 +29,8 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
     @Autowired TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager entityManager;
 
     @Test
     public void testMember() {
@@ -133,4 +141,111 @@ class MemberRepositoryTest {
             System.out.println("dto = " + dto);
         }
     }
+
+    @Test
+    public void findByNames() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByNames(List.of(member1.getUsername(), member2.getUsername()));
+
+        assertThat(result).isEqualTo(List.of(member1, member2));
+    }
+
+    @Test
+    public void returnType() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Optional<Member> result = memberRepository.findOptionalByUsername("asdqw");
+        System.out.println("list.get() = " + result );
+        
+    }
+
+    @Test
+    public void paging() {
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age =10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by("username").descending() );
+        // Data Jpa 의 페이징은 0번 부터 시작
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        //Page는 토탈카운트+페이지, Slice 는 Page+1, List는 조건에 맞는 것만
+        // slice는 다음 페이지가 있는지 없는지 정도로만 페이징을 한다.
+        // 통상적인 페이징은 page로
+
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        // entity -> dto 로 변활할수 있는 map 메서드 제공
+
+        List<Member> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        // 현재 페이지의 크기
+        assertThat(totalElements).isEqualTo(5);
+        // 전체 크기
+        assertThat(page.getNumber()).isEqualTo(0);
+        // 현재 페이지 번호 0번부터 시작
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        // 전체 페이지 수
+        assertThat(page.isFirst()).isTrue();
+        // 현재 페이지가 첫장 인지
+        assertThat(page.hasNext()).isTrue();
+        // 다음 페이지가 있는지
+    }
+
+    @Test
+    public void bulkUpdate() {
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        int resultC0unt = memberRepository.bulkAgePlus(20);
+        // 벌크 연산은 영속성 컨텍스트를 무시하고 쿼리들을 flush 시켜버리기 떄문에 영속성 컨텍스트의 값과 DB의 값이 불일치 하게 될수 있다.
+        // 때문에 flush 되었다면 영속성 컨텍스트를 초기화 하고 다시 db에서 값을 가져와야 동일성이 보장된다
+        // 하지만 Data JPA 는 Modifying 어노테이션에서 자동 클리어 옵션을 제공한다!
+//        entityManager.flush();
+//        entityManager.clear();
+        // 그래서 벌크연산 실행 이후 영속성 컨텍스트를 초기화 하는것이 좋다
+
+        List<Member> result = memberRepository.findListByUsername("member5");
+        Member member5 = result.get(0);
+        System.out.println("member5 = " + member5);
+
+        assertThat(resultC0unt).isEqualTo(3);
+    }
+    
+    @Test
+    public void findMemberLazy() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+        for (Member member : members) {
+            System.out.println("member.getUsername() = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
+        }
+    }
+
 }
